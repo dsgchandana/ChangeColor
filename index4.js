d59316e788a64c5f162460c95ab5362a7781c27c
dsgchandana <143314207+dsgchandana@users.noreply.github.com>
2024-01-10 11:44:39 +0530

    Add files via upload

--- index4.js
+++ index4.js
@@ -2,7 +2,7 @@ const button=document.querySelector("button");
 const body=document.querySelector("body");
 const color=["indiogo","blue","pink","red","green","yellow","orange"];
 body.style.backgroundColor='black';
-document.addEventListener('click',function(){
+document.querySelector("button").addEventListener('click',function(){
     const colorIndex=parseInt(Math.random()*color.length);
     body.style.backgroundColor=color[colorIndex];
 
